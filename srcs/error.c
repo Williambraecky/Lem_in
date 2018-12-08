5cc9bc8dfa4f8352a532ce7600383d1dfe4618b3
William Braeckman <wbraeckm@student.s19.be>
2018-12-08 12:53:01 +0100

    Free hash table on error

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 13:51:39 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/12/06 16:26:31 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/12/07 00:42:11 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	error_exit(t_lem *lem)
 	ft_printf("ERROR\n");
 	if (lem && lem->rooms)
 		free_rooms(lem);
+	if (lem && lem->hash_table)
+		free(lem->hash_table);
 	if (lem && lem->solutions)
 		ft_lstdel(&lem->solutions, del_combo);
 	if (lem && lem->solve)
